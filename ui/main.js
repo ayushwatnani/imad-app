ee25cd84dbea3b92752356357e62c96d26b18672
Ayush Watnani <watnaniayush@gmail.com>
2017-08-08 20:52:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ button.onclick = function () {
             if(request.status == 200){
                 var counter = request.responseText;
                 var span = document.getElementById('count');
-                innerHTML = counter.toString();
+                span.innerHTML = counter.toString();
             }
         }
     };
