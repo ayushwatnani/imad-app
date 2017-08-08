562f16d787acd31f07621990e59af3105a73a602
Ayush Watnani <watnaniayush@gmail.com>
2017-08-08 13:32:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ element.innerHTML = 'New value';
 var i = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     i.style.marginLeft = marginLeft + 'px';
 }
 
