4efd30af72d91814f36276c360d5575e11a9b49b
Ayush Watnani <watnaniayush@gmail.com>
2017-08-08 21:15:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@ button.onclick = function () {
     
     //Make the request.
     request.open('GET', 'http://ayushwatnani.imad.hasura-app.io/counter', true);
-    request.send(null);
+    request.send();
    
    
 };
