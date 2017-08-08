937375a45ac1abe2ac166bc649f3baf433fa013f
Ayush Watnani <watnaniayush@gmail.com>
2017-08-08 10:51:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,8 +77,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req, res){
-    res.send(createTemplate(article-one));
+app.get('/:articleName', function(req, res){
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/article-two', function(req, res){
