3a4ea9abb2ce175f0e97bbb055a73534b589aba2
Ayush Watnani <watnaniayush@gmail.com>
2017-08-08 13:17:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 // Move the image
-var img = document.getEelemntById('madi');
+var img = document.getEelementById('madi');
 img.onclick = function(){
     img.style.marginLeft = '500px';
 }
