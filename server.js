2331005086d21098f241c0731c4f56882dcebdb5
Ayush Watnani <watnaniayush@gmail.com>
2017-08-10 12:31:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name', function(req, res){
+app.get('/submit-name/:name', function(req, res){
     // GET the name from the request
     var name = req.params.name;
     names.push(name);
