d41a00037fc04fa2e18ce98aa9cebf7ca42f3396
Ayush Watnani <watnaniayush@gmail.com>
2017-08-24 19:12:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,4 @@
-//counter
-var button = document.getElementById('counter');
-
-button.onclick = function () {
-    // make a request to counter end point.
-     var request = new XMLHttpRequest();
-     
-    //capture the response and store it in a variable.
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            // take some action
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    
-    //Make the request.
-    request.open('GET', 'http://ayushwatnani.imad.hasura-app.io/counter', true);
-    request.send(null);
-   
-   
-};
-
-//Submit name
+//Submit username/password to login 
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
@@ -37,20 +11,22 @@ submit.onclick = function(){
           //take some action
           if(request.status === 200){
               //capture a list of names and render it as a list
-              var names = request.responseText;
-              names = JSON.parse(names);
-              var list ='';
-              for(var i=0; i<names.length;i++){
-                  list += '<li>'+ names[i] +'</li>';
-              }
-              var ul = document.getElementById('namelist');
-              ul.innerHTML = list;   
+             console.log('user logged in');
+             alert('Logged in successfully');
+          }else if(request.status === 403){
+              alert('username/password incorrect');
+          }else if(request.status === 500){
+              alert('something went wrong on the server');
           }
       }
   };
   
-  var nameInput = document.getElementById('name');
+  var username = document.getElementById('username');
+  var password = document.getElementById('password');
+  console.log(username);
+  console.log(password);
   var name = nameInput.value;
-  request.open('GET', 'http://ayushwatnani.imad.hasura-app.io/submit-name?name='+ name, true);
-  request.send(null);
+  request.open('POST', 'http://ayushwatnani.imad.hasura-app.io/login', true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({username: username, password: password}));
 };
