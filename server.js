ab5dda54e9ec3915b1fed9a11102d0a297cfcbbd
Ayush Watnani <watnaniayush@gmail.com>
2017-08-24 18:41:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,30 @@ app.post('/create-user', function(req, res){
     });
 });
 
+app.post('/login',function (req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+  
+    pool.query('SELECT * FROM "user" $1', [username], function(err, result){
+        if(err){
+            res.status(500).send(err.toString());    
+        }else {
+            if(results.rows.length ===0){
+                res.send(403).send('username/password is invalid')
+            }else{
+                var dbString = result.row[0].password;
+                var salt = dbString.split('$')[2];
+                var hashedPassword = hash(Password, salt)
+                if(hashedPassword === dbString){
+                    res.send('credentials correct');
+                }else{
+                    res.send(403).send('username/password is invalid');
+                }
+             }
+        }
+    });
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function(req, res){
     pool.query('SELECT * FROM test', function(err, result){
