9bdadd6d65729442743e7d2c3d0fd5a6397161c1
Ayush Watnani <watnaniayush@gmail.com>
2017-08-24 18:49:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.post('/login',function (req, res){
             if(results.rows.length ===0){
                 res.send(403).send('username/password is invalid')
             }else{
-                var dbString = result.row[0].password;
+                var dbString = result.rows[0].password;
                 var salt = dbString.split('$')[2];
                 var hashedPassword = hash(password, salt)
                 if(hashedPassword === dbString){
